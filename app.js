55fc3537f7f08eba6a9aa4ca05086ee2f819e55e
Tamari Giorgobiani <takogiorgobiani97@gmail.com>
2022-02-06 13:29:31 +0400

    no message

--- app.js
+++ app.js
@@ -59,16 +59,22 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+ const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const item = new Item( {
+    name: itemName
+  });
+  
+  item.save();
+  res.redirect("/");
+
 });
+
+ app.post("/delete", function(req,res){
+   console.log(req.body.checkbox);
+ });
+
+
 
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
